d28aa280df02c3320d1d90776eadc6fc6b96310a
Jason Sprouse <jason.a.sprouse@gmail.com>
2019-01-15 18:21:32 -0600

    removed comments in Post resolver

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -126,7 +126,7 @@ const typeDefs = `
         body: String!
         published: Boolean!
         author: User!
-        comments: [Comment!]!
+        comments: [Comment]!
     }
     type Comment {
         id: ID!
@@ -178,11 +178,6 @@ const resolvers = {
                 return user.id === parent.author
                 })
             }
-        },
-        comments(parent, args, ctx, info){
-            return comments.filter((comment) => {
-                return comment.post === parent.id
-            })
         },
     User: {
         posts(parent, args , ctx, info){
