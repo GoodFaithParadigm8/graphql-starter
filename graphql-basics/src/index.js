da56e38c91fcf20b985d1613b71282c18a4dbbb5
Jason Sprouse <jason.a.sprouse@gmail.com>
2019-01-17 14:16:21 -0600

    added spread operator to Mutation operations

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -155,9 +155,7 @@ const resolvers = {
 
             const user = {
                 id: uuidv4(),
-                name: args.name,
-                email: args.email,
-                age: args.age
+                ...args
             }
 
             users.push(user)
@@ -174,10 +172,7 @@ const resolvers = {
 
             const post = {
                 id: uuidv4(),
-                title: args.title,
-                body: args.body,
-                published: args.published,
-                author: args.author
+                ...args
             }
 
             posts.push(post)
@@ -206,9 +201,7 @@ const resolvers = {
 
             const comment = {
                 id: uuidv4(),
-                text: args.text,
-                author: args.author,
-                post: args.post
+               ...args
             }
 
             comments.push(comment)
